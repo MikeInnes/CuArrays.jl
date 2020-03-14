2a96cacc11242db96d53b975075d396661509886
matsueushi <matsueushi@gmail.com>
2020-03-14 20:23:28 +0000

    Remove wrappers

--- src/nnlib.jl
+++ src/nnlib.jl
@@ -1,13 +1,9 @@
 using NNlib
 
 # Activation functions
-@cufunc σ(x::Real) = ifelse(x < -80, zero(x), one(x) / (one(x) + exp(-x)))
-
 @cufunc softplus(x::Real) = ifelse(x > 0, x + log1p(exp(-x)), log1p(exp(x)))
 
 @cufunc logσ(x::Real) = -softplus(-x)
-
-@cufunc elu(x::Real, α = one(x)) = ifelse(x ≥ 0, x / one(x), α * (exp(x) - one(x)))
 
 @cufunc function gelu(x::Real)
     p = oftype(x / 1, π)
@@ -17,23 +13,14 @@ using NNlib
     h * x * (one(x) + tanh(λ * (x + α * x^3)))
 end
 
-@cufunc swish(x::Real) = x * σ(x)
-
 @cufunc lisht(x::Real) = x * tanh(x)
-
-@cufunc function selu(x::Real)
-    λ = oftype(x / 1, 1.0507009873554804934193349852946)
-    α = oftype(x / 1, 1.6732632423543772848170429916717)
-    λ * ifelse(x > 0, x / one(x), α * (exp(x) - one(x)))
-end
-
-@cufunc celu(x::Real, α::Real = one(x)) = ifelse(x ≥ 0, x / one(x), α * (exp(x/α) - one(x))) 
 
 @cufunc logcosh(x::Real) = x + softplus(-2x) - log(oftype(x, 2))
 
 @cufunc mish(x::Real) = x * tanh(softplus(x))
 
 @cufunc tanhshrink(x::Real) = x - tanh(x)
+
 
 # Batched matrix multiplication
 
